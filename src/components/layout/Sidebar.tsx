7814156357613251f0ac7e9ea178a11f42f49e76
Andre P Alves <andre.p.a.8.2001@gmail.com>
2026-06-10 13:30:01 -0300

    V0.5.0 Merges backend branch

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -16,7 +16,7 @@ export function Sidebar() {
       </nav>
 
       <div className="text-rema-tan/40 text-xs text-center border-t border-rema-dark-mid pt-4">
-        v0.3.0
+        v0.5.0
       </div>
     </aside>
   );
